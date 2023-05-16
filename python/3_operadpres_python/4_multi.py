2083f3a94543b5657a283f61834678fbd4e8fc6a
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 07:58:10 -0400

    Create 4_multi.py

---
+++ python/3_operadpres_python/4_multi.py
@@ -0,0 +1,9 @@
+num1= 100
+num2= 200
+
+result =  num1 * num2
+
+print('El resultado de la Multiplicacion es: ' , result)
+print('El resultado de la Multiplicacion es: ', num1 * num2)
+print(f'el Resultado unsado format {result}:')
+print("Esta es otra format de Multiplicacion y poner el resultado {}".format(result))
