f3c22a0d939021a190a83032fc5e98f2a2d20ed0
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-15 23:45:45 -0400

    Create 8_funcion_id.py

---
+++ python/1_variables_y_operadores/8_funcion_id.py
@@ -0,0 +1,13 @@
+# id = sirve para ver el valor donde esta almacenada en memoria
+
+x = 10
+y = 11
+z = x + y
+
+print(x + y)
+print(z)
+
+#valor de en memoria 
+print(id(x))
+print(id(y))
+print(id(z))
