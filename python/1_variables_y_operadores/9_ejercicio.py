ffe12179999ee050e969e609fd56b5623ef51e00
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-15 23:45:50 -0400

    Create 9_ejercicio.py

---
+++ python/1_variables_y_operadores/9_ejercicio.py
@@ -0,0 +1,21 @@
+#Imprimir 3 variables: Nombre, telefono, email
+
+name = "Natanael"
+phone = "8092105513"
+email = "Natanaelsosagmail.com"
+
+#imprimir individual
+print(name)
+print(phone)
+print(email)
+print("\n") # espacion de una a otra linea
+#------------------------------------
+# imprimir individual con catenacion
+print("Name = "+ name)
+print("Phone = "+ phone)
+print("Email = "+ email)
+print("\n") # espacion de una a otra linea
+#----------------------------------------
+# con catenacion
+print("Name = " + name + ", Email = "+ email +", phone = "+ phone)
+
