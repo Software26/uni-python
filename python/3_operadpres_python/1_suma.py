14a9ba920edd01fece462aee2da66dc8b1b5baeb
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 07:57:49 -0400

    Create 1_suma.py

---
+++ python/3_operadpres_python/1_suma.py
@@ -0,0 +1,9 @@
+num1= 100
+num2= 200
+
+result =  num1 + num2
+
+print('El resultado de la Suma es: ' , result)
+print('El resultado de la Suma es: ', num1 + num2)
+print(f'el Resultado unsado format {result}:')
+print("Esta es otra format de sumar y poner el resultado {}".format(result))
