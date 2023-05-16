f5c021c7ce18d99cc8f330c9f6fbc015182cacc5
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 07:57:56 -0400

    Create 2_resta.py

---
+++ python/3_operadpres_python/2_resta.py
@@ -0,0 +1,9 @@
+num1= 100
+num2= 200
+
+result =  num1 - num2
+
+print('El resultado de la RESTA es: ' , result)
+print('El resultado de la RESTA es: ', num1 - num2)
+print(f'el Resultado unsado format {result}:')
+print("Esta es otra format de RESTA y poner el resultado {}".format(result))
