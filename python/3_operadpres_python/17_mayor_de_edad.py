b3e69952c0eac6810691111efcd97de1f68ac7c5
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 22:40:58 -0400

    Create 17_mayor_de_edad.py

---
+++ python/3_operadpres_python/17_mayor_de_edad.py
@@ -0,0 +1,10 @@
+
+adulto = 18
+edad = int(input("Digite su edad: "))
+
+if edad >= adulto:
+    print("Es mayor de edad pase, si puede pasar")
+    
+else:
+    print("Es menor de edad, no puede pasar")
+    
