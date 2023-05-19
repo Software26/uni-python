278cfc7b77388dd08c16ab3290587262c81e153c
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-18 20:20:16 -0400

    Create 22_operador_or.py

---
+++ python/3_operadpres_python/22_operador_or.py
@@ -0,0 +1,19 @@
+edad = int(input("Digite su edad: "))
+meses = int(input("Digite de su nacimento en numero: "))
+mesactual = int(input("Digite el numero del mes actual: "))
+
+if  (mesactual >= meses):
+    cantidadmeses =  mesactual - meses
+    if (edad == 17 and meses >= 7):
+        print(f" Tu edad es {edad} y tienes {cantidadmeses}Puedes pasar ya casi erers mayor de edad")
+
+else:
+    cantidad = meses - mesactual
+    if e: 
+    
+    
+    
+    
+
+if (edad == 17 and meses >= 7):
+    print()
