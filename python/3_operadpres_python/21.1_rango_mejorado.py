c8ce4736efd5dbc436b2ea3d0c50f90a68edae7d
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-18 20:13:14 -0400

    Create 21.1_rango_mejorado.py

---
+++ python/3_operadpres_python/21.1_rango_mejorado.py
@@ -0,0 +1,9 @@
+valor = int(input("Escribe el valor: "))
+valorMInimo = 0
+valorMaximo = 5
+
+if (valor >= valorMInimo)and (valor<= valorMaximo):
+    print("Esta dentro del rango")
+    
+else: 
+    print("Esta fuera del rango")
