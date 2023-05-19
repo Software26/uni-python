ccb4979b043de58088d0e95c3532767a34a952a9
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-19 08:48:04 -0400

    Delete 22_operador_or.py

--- python/3_operadpres_python/22_operador_or.py
+++
@@ -1,8 +0,0 @@
-edad = int(input("Digite su edad: "))
-mesactual = int(input("Digite el numero del mes actual: "))
-
-if (edad >=17):
-    print(f" Tu edad es {edad} y tienes {mesactual}Puedes pasar ya casi erers mayor de edad")
-
-else:
-    print(f"No, cumples con la edad requerida, tiene {edad} los requisitos minimos son de 17 anios")
