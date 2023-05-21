0768cb2efb824fc594814697be4e6e21b1a10b79
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 09:53:56 -0400

    Create 25_operador_not.py

---
+++ python/3_operadpres_python/25_operador_not.py
@@ -0,0 +1,8 @@
+vacaciones =  True
+diaDescanso = False
+
+#not cuando no referimos a not es lo puesto de que se tiene
+if not(vacaciones or diaDescanso):
+    print("Puede asistir Al juego")
+else:
+    print("Tiene deveres que hacer")
