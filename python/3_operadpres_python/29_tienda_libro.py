e01cd1a297f0032e26b9ca1fea6c52845f64c605
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 13:24:20 -0400

    Update 29_tienda_libro.py

--- python/3_operadpres_python/29_tienda_libro.py
+++ python/3_operadpres_python/29_tienda_libro.py
@@ -1,10 +1,23 @@
+print("###############################################################")
+print("###################   Tienda Producto #########################")
+print("###############################################################\n")
+
 print("Propocione los siguientes datos del libro: ")
 nombreLibro = (input("Propociona el nombre: "))
 idlibro = int(input("Propociona el ID: "))
-precio = float(input("Propocina el precio: "))
-indicacion = str(input("Indica si el envio es gratuito (Si/No)"))
+precio = float(input("Propocina el precio: (Formato decimal) "))
+indicacion = str(input("Indica si el envio es gratuito (True/False) "))
 
-print(nombreLibro)
-print(idlibro)
-print(precio)
-print(indicacion)
+if indicacion == True:
+    indicacion = "True"
+
+elif indicacion == False:
+    indicacion = "False"
+else:
+    indicacion =  "El valor es incorrecto Favor de digitar True/False"
+print("-----------------------------------------------------------")
+print(f"Nombre: {nombreLibro}")
+print(f"Id: {idlibro}")
+print(f"Precio: {precio}")
+print(f"Envio Gratuito: {indicacion}")
+print("-----------------------------------------------------------")
