76c6c13492c7d51e43cb9ef73b0d6b00cf5727ea
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 09:54:01 -0400

    Create 24_operador_or.py

---
+++ python/3_operadpres_python/24_operador_or.py
@@ -0,0 +1,9 @@
+# Realizar un programa que diga si puede ir al partido de su hijo.
+
+vacaciones =  True
+diaDescanso = False
+
+if vacaciones or diaDescanso:
+    print("Puede asistir Al juego")
+else:
+    print("Tiene deveres que hacer")
