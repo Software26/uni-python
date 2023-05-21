d8aec3de738e6504774c697cbf45a6da7e114182
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 13:02:11 -0400

    Create 26_rango_entre_20_y_30.py

---
+++ python/3_operadpres_python/26_rango_entre_20_y_30.py
@@ -0,0 +1,9 @@
+# Rango entre los 20 o los 30
+
+edad = int(input("Digirye su edad"))
+
+if (edad >= 20) and (edad <= 30):
+    print("Su edad de {edad} esta en el rango ")
+
+else:
+    print("Su edad no esta en el rango")
