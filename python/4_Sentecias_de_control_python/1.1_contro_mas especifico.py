07dc62c1a25f5647e4880657a9fffcbf9f5fce4d
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 14:17:36 -0400

    Create 1.1_contro_mas especifico.py

---
+++ python/4_Sentecias_de_control_python/1.1_contro_mas especifico.py
@@ -0,0 +1,11 @@
+condicion =  False 
+
+if condicion == True:
+    print("Condicion o True ")
+    
+elif condicion == False:
+    print("Condicion Falsa")
+    
+else:
+    print("Condicion no conocida")
+    
