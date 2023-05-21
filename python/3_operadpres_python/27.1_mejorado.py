33a11f13a83303d4950addc48028506782a98271
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 13:02:40 -0400

    Create 27.1_mejorado.py

---
+++ python/3_operadpres_python/27.1_mejorado.py
@@ -0,0 +1,12 @@
+edad =  int(input("Digite su edad: "))
+
+if (edad >=20) and (edad < 30):
+    print(f" Su edad es {edad} esta en rango de lo veiinte anios")
+elif (edad >= 30) and (edad <40 ):
+    print(f" Su edad es {edad} esta en rango de lo 30 anios")
+elif (edad >=40)and (edad <50):
+    print(f" Su edad es {edad} esta en rango de lo 40 anios")
+elif (edad >=40)and (edad <50): 
+    print(f" Su edad es {edad} esta en rango de lo 50 anios")
+else:
+    print("Su edad no esta en ningun de los rango")
