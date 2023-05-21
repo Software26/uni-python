37ad93ba0988da80e31b4480308df7a84f687d6b
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 14:16:57 -0400

    Create 1.0_funcion_type.py

---
+++ python/2_Tipos de datos/1.0_funcion_type.py
@@ -0,0 +1,25 @@
+num = 10
+num1 = 10.5
+letras = "Hola"
+booleanaos1 = True
+booleanaos2 = False
+
+#Impresion comun por cosola
+print(num)
+print(letras)
+print(booleanaos1)
+print(booleanaos2)
+print("\n")
+
+#tipos con type()
+print(num)
+print(type(num))
+print(num1)
+print(type(num1))
+print(letras)
+print(type(letras))
+print(booleanaos1)
+print(type(booleanaos1))
+print(booleanaos2)
+print(type(booleanaos2))
+
