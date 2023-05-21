594929bcea2554342b7c8ea36b2261ef6810928b
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-21 13:02:15 -0400

    Create 29_tienda_libro.py

---
+++ python/3_operadpres_python/29_tienda_libro.py
@@ -0,0 +1,10 @@
+print("Propocione los siguientes datos del libro: ")
+nombreLibro = (input("Propociona el nombre: "))
+idlibro = int(input("Propociona el ID: "))
+precio = float(input("Propocina el precio: "))
+indicacion = str(input("Indica si el envio es gratuito (Si/No)"))
+
+print(nombreLibro)
+print(idlibro)
+print(precio)
+print(indicacion)
