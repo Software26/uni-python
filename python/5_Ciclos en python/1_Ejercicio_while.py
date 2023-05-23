07cbd88d2b0de7c0b182a1f39adbcedba6ce2fb8
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-22 23:23:47 -0400

    Create 1_Ejercicio_while.py

---
+++ python/5_Ciclos en python/1_Ejercicio_while.py
@@ -0,0 +1,19 @@
+# Imprimir los números enteros del 0 al 5 utilizando un ciclo while
+
+contador = 0
+
+while contador <=5  :
+    print(contador)
+    contador += 1
+else:
+    print("fin ciclo while")
+    
+"""
+Salida 
+0
+1
+2
+3
+4
+5
+"""
