5b7980a2e8cb4a76af7160759ebc11c5643c0764
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-22 23:37:12 -0400

    Create 2_decremento_Ejercicio_while.py

---
+++ python/5_Ciclos en python/2_decremento_Ejercicio_while.py
@@ -0,0 +1,19 @@
+# Imprimir los números enteros del 5 al 1 de forma decrementando utilizando un ciclo while
+minimo = 1
+maximo = 5
+
+
+while maximo >= minimo  :
+    print(maximo)
+    maximo -= 1
+else:
+    print("fin ciclo while")
+    
+"""
+Salida 
+5
+4
+3
+2
+1
+"""
