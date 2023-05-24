365de8d400076f86c4fc79cfde7968178da6a054
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-24 18:54:17 -0400

    Create 6.0_remover_element_lista.py

---
+++ python/6_Coleccions en python/6.0_remover_element_lista.py
@@ -0,0 +1,9 @@
+nombres = ['Juan','Karla','Ricardo', 'María']
+
+#----Insert element---
+nombres.insert(1, "Octavio")
+print(nombres)
+
+#--Remove un elemento--
+nombres.remove("Octavio")
+print(nombres)
