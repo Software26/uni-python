4614198ee6cc5a2a033a02f303bd808406df66f1
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-24 18:26:03 -0400

    Create 1_Lista.py

---
+++ python/6_Coleccions en python/1_Lista.py
@@ -0,0 +1,10 @@
+# Definir una lista de tipo str
+nombres = ['Juan','Karla','Ricardo', 'María']
+# imprimir la lista nombres
+print(nombres) 
+# acceder a los elementos de un a lista
+print(nombres[0]) # el primer elemento
+print(nombres[1]) # segundo Elemento
+# acceder a los elementos de manera inversa
+print(nombres[-1])# cuarto elemento
+print(nombres[-2]) # tercer elemento
