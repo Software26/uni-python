622ad5f66b6a7a72c944cf8c8fcc0854735e7798
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-24 23:27:05 -0400

    Update 12_Tuplas2.py

--- python/6_Coleccions en python/12_Tuplas2.py
+++ python/6_Coleccions en python/12_Tuplas2.py
@@ -1,1 +1,7 @@
 frutas =("Naranja", "Platano","Guyaba")
+# recorre elementos uno debajo del otro
+for frurta in frutas:
+    print(frurta)
+#Recorre elementes en una fila y al agregar un str de algun separador   
+for frurta in frutas:
+    print(frurta, end=("-"))
