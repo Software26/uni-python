4a7d06f5f8aa67532e2bac9ea51fc90df0148219
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-25 06:56:00 -0400

    Create 15.2diccionario.py

---
+++ python/6_Coleccions en python/15.2diccionario.py
@@ -0,0 +1,10 @@
+diccionario = {
+    "IDE": 'Integrade Development Environment',
+    "OPP": "object Orientad Prrograming",
+    "DBMS":"Database Management Sytem" 
+    
+}
+
+# comprobar existencia de algun elemento
+print('IDE' in diccionario)
+print()
