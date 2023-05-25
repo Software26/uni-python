0eabcc3b8a23cb8150b0714df780cfee589bdf55
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-24 23:33:49 -0400

    Create 14_Sets.py

---
+++ python/6_Coleccions en python/14_Sets.py
@@ -0,0 +1,10 @@
+planeta = {"Marte", "Venus","Jupiter"}
+print(planeta)
+
+#largo
+print(len(planeta))
+
+#revisar elemento presente en el set
+print("Marte" in planeta)
+print("Tierra" in planeta)
+
