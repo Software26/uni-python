6ce86e2942ff1632711458efc0d4a5386c7f5729
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-28 22:00:10 -0400

    Delete 1_funciones.py

--- python/7_funciones.py/1_funciones.py
+++
@@ -1,7 +0,0 @@
-def mifuncion(nombre,apellido):
-    print("Saludos desde mi funcion")
-    nombre = str(input("Digite un su nombre: "))
-    apellido = str(input("Digite su apellido: "))
-    print(f"Nombre:{nombre}, Apellido: {apellido}")
-    
-mifuncion("a","b")
