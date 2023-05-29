c3a33120435e5a5caab0616d7170a7819251c27a
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-28 22:34:00 -0400

    Create 2_funcion.py

---
+++ python/7_funciones.py/2_funcion.py
@@ -0,0 +1,6 @@
+def mi_funcion(nombre, apellido):
+    print('Saludos desde mi funcion')
+    print(f"Nombre: {nombre}, Apellido: {apellido}")
+    
+mi_funcion("Juan","Perez")
+mi_funcion("Nata","Sosa")
